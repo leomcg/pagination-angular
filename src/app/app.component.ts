c663138d3120afab928e0b0f76f391de4e088e1b
Leonardo Gonçalves <leomcg@gmail.com>
2020-06-08 12:02:03 -0300

    practicing directives

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  currentPage = 0;
+  images = [
+    {
+      title: 'At the beach',
+      imageUrl: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'Soccer stadium',
+      imageUrl: 'https://images.unsplash.com/photo-1504016798967-59a258e9386d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'Skateboading',
+      imageUrl: 'https://images.unsplash.com/photo-1450644995374-1721affb8ecd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'Snowboarging',
+      imageUrl: 'https://images.unsplash.com/photo-1565733362858-3c610e8044cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    }
+  ];
+
+  onPageChange(index: number) {
+    this.currentPage = index;
+  }
+}
